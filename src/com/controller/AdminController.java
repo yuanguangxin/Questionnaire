c9c18bcc419bb79eb434d52cc7d7179772a92d45
yuanguangxin <274841922@qq.com>
2016-06-10 23:27:35 +0800

    add login

--- src/com/controller/AdminController.java
+++ src/com/controller/AdminController.java
@@ -1,13 +1,34 @@
 package com.controller;
 
+import com.models.Admin;
+import com.service.AdminService;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class AdminController {
-    @RequestMapping("/test")
-    public String test(){
-        return "/index.jsp";
+    private AdminService adminService;
+
+    public AdminService getAdminService() {
+        return adminService;
+    }
+
+    public void setAdminService(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @RequestMapping("/login")
+    public void login(Admin ad, HttpServletResponse response, HttpSession session) throws Exception{
+        Admin admin = adminService.login(ad);
+        if(admin==null){
+            response.getWriter().print("wrong");
+        }else {
+            session.setAttribute("admin",admin);
+        }
     }
 }
--- src/com/service/AdminService.java
+++ src/com/service/AdminService.java
@@ -1,6 +1,9 @@
 package com.service;
 
 import com.mapper.AdminMapper;
+import com.models.Admin;
+import com.models.AdminExample;
+import java.util.List;
 
 public class AdminService {
     private AdminMapper adminMapper;
@@ -12,8 +15,13 @@ public class AdminService {
     public void setAdminMapper(AdminMapper adminMapper) {
         this.adminMapper = adminMapper;
     }
+
+    public Admin login(Admin admin){
+        AdminExample adminExample = new AdminExample();
+        AdminExample.Criteria criteria = adminExample.createCriteria();
+        criteria.andUsernameEqualTo(admin.getUsername()).andPasswordEqualTo(admin.getPassword());
+        List<Admin> list = adminMapper.selectByExample(adminExample);
+        if(list.size()==0) return null;
+        else return list.get(0);
+    }
 }
-
-
-
-
